6293a15902ea6a790a97123fb6507a5b9192a575
Juliannn <flbloodheist@gmail.com>
2026-06-13 11:12:19 +0800

    asdasd

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
             <Route path="/game" element={<Game />} />
           </Routes>
         </div>
-      </BrowserRouter>
+      </HashRouter>
     </GameProvider>
   );
 }
